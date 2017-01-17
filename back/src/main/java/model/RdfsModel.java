b72b34521a93b0e2d90a42023c1c67c6ee934d22
Achraf <nasri.ach@gmail.com>
2017-01-17 15:19:56 +0100

    first commit

---
+++ back/src/main/java/model/RdfsModel.java
@@ -0,0 +1,90 @@
+package model;
+
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.ontology.Ontology;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+public class RdfsModel {
+
+	private static final String FAMILY_NAMESPACE = "http://familytree/ns/";
+	private static final String MEMBER_NAMESPACE = "http://familytree/member/ns/";
+	
+	private static OntModel model;
+	
+	OntClass family;
+	OntClass member;
+	
+	//************* PROPERTIES FOR FAMILY ***************
+	//***************************************************
+	public static Property hasId;
+	public static OntProperty hasCreator;
+	
+	
+	//************* PROPERTIES FOR MEMBER ***************
+	//***************************************************
+	public static OntProperty hasFistName;
+	public static OntProperty hasLastName;
+	public static OntProperty hasBirthDate;
+	public static OntProperty hasAddress;
+	public static OntProperty hasProfession;
+	public static OntProperty hasPhone;
+	public static OntProperty hasEmail;
+	public static OntProperty hasGender;
+	
+	public static OntProperty hasSibling;
+	public static OntProperty hasParent;
+	public static OntProperty hasChild;
+	public static OntProperty hasSpouse;
+
+	static {
+		model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RDFS_INF);
+
+		OntClass family = model.createClass( FAMILY_NAMESPACE + "Family" );
+		OntClass member = model.createClass( MEMBER_NAMESPACE + "Member" );
+		//Ontology o = model.createOntology(FAMILY_NAMESPACE);
+		//family.add
+		
+		hasCreator = model.createObjectProperty(FAMILY_NAMESPACE + "hasCreator");
+		//hasCreator.addRange(res);
+		hasId = model.createProperty(MEMBER_NAMESPACE, "hasId");
+		hasFistName = model.createObjectProperty(MEMBER_NAMESPACE + "hasFistName");
+    	hasLastName = model.createObjectProperty(MEMBER_NAMESPACE + "hasLastName");
+    	hasBirthDate = model.createObjectProperty(MEMBER_NAMESPACE + "hasBirthDate");
+    	hasAddress = model.createObjectProperty(MEMBER_NAMESPACE + "hasAddress");
+    	hasProfession = model.createObjectProperty(MEMBER_NAMESPACE + "hasProfession");
+    	hasPhone = model.createObjectProperty(MEMBER_NAMESPACE + "hasPhone");
+    	hasEmail = model.createObjectProperty(MEMBER_NAMESPACE + "hasEmail");
+    	hasGender = model.createObjectProperty(MEMBER_NAMESPACE + "hasGender");
+    	
+    	
+    	hasSibling = model.createObjectProperty(MEMBER_NAMESPACE + "hasSibling");
+    	hasSpouse = model.createObjectProperty(MEMBER_NAMESPACE + "hasSpouse");
+    	hasParent = model.createObjectProperty(MEMBER_NAMESPACE + "hasParent");
+    	hasChild = model.createObjectProperty(MEMBER_NAMESPACE + "hasChild");
+    	
+    	hasParent.setInverseOf(hasChild);
+    	hasSibling.setInverseOf(hasSibling);
+    	
+
+	}
+	
+	public static Model getModel() {
+		return model;
+	}
+
+	public static Resource addMember(Person member) {
+		String memberURI = "http://familytree/person/" + member.getFirstName() + member.getLastName();
+		Resource memberResource = model.createResource(memberURI);
+		
+		memberResource.addProperty(hasFistName, "achraf");
+		memberResource.addProperty(hasLastName, "nasri");
+		return memberResource;
+	}
+	
+}
---
+++ back/src/main/java/services/FamilyService.java
@@ -0,0 +1,66 @@
+package services;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.ws.rs.core.MediaType;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Family;
+import model.Person;
+import model.RdfsModel;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value="/family")
+public class FamilyService {
+
+	@RequestMapping(value="/create", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+    public String testFramework(@RequestBody Family family) {
+
+    	String familyID = UUID.randomUUID().toString();
+    	String familyURI = "http://familytree/" + familyID;
+
+    	Model model = RdfsModel.getModel();
+
+    	// create the resource
+    	Resource familyResource = model.createResource(familyURI);
+    	Resource creatorResource = RdfsModel.addMember(family.getCreator());
+    	familyResource.addProperty(RdfsModel.hasCreator, creatorResource);
+    	familyResource.addProperty(RdfsModel.hasId, familyID);
+    	
+    	model.write(System.out);
+    	
+    	return familyID;
+    }
+	
+	@RequestMapping(value="/auth/{id}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+    public List<String> authenticate(@PathVariable String id) {
+		// check if id exists in the RDF (if family exists)
+		// if exists
+			//get from rdf all members of this family in the format (firstName + lastName)
+			// return this list of Strings
+		// else
+			// return error message
+		return null;
+    }
+	
+	@RequestMapping(value="{id}/add", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+    public void addMember(@RequestBody Person person, @PathVariable String id) {
+		// get family from rdf
+		// get the member specified in person from family let's call it A
+		// create the person
+		// make the relation between person and A
+		// update the RDF
+		
+    }
+}
